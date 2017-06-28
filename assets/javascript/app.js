3f08d86df456df1e245d08f3978fca2242294c51
kmgulledge <kmgulledge@gmail.com>
2017-06-28 06:36:51 -0400

    update 2

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Create a function that creates the start button and initial screen
+
+// Set up Clock
+
+// Set up Questions
+
+// Set up Correct Answers
+
+// Set up Wrong Answers
+
+// Set up Unanswered
+
+// Set up Restart Button
+
---
+++ ideas.md
@@ -0,0 +1,10 @@
+1. how many questions?
+10
+
+2. what type of theme?
+goonies
+
+3. html set up?
+bootstrap
+
+4. javascript / jquery help.....
