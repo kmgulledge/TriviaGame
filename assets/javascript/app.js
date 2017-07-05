a7cab01ed07bc16bb08df7badb02a4771fb4eb80
kmgulledge <kmgulledge@gmail.com>
2017-07-05 06:23:45 -0400

    updates galore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,16 +14,35 @@ function populate() {
         for(var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
         }
+
+        showProgress();
     }
 };
 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function(){
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function showProgress(){
+    var currentQuestionNumber = quiz.questionIndex = 1;
+    var element = document.getElementById("progress");
+    element.innerHTML - "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+}
+
+
+
 function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
-}
+};
 
 // Question Variable
 var questions = [
@@ -40,6 +59,5 @@ var questions = [
     ];
 
     var quiz = new Quiz(questions);
-    console.log()
 
     populate();
